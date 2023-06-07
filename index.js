c1ff275aed5d1e9ae25932806b17fc107e5c0b12
Alex Worthington <Alex.Worthington@softwire.com>
2023-06-07 11:17:15 +0100

    Added a colour-palette toggle to homepage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const hiddenButton = document.querySelector("button");
+const hiddenButton = document.getElementById("hidden-button");
 
 hiddenButton.onclick = function () {
     hiddenButton.classList.add("hide");
@@ -9,3 +9,31 @@ hiddenButton.onclick = function () {
     const hiddenVideo = document.getElementById("sport-video");
     hiddenVideo.classList.remove("hide");
 };
+
+const colourPaletteToggle = document.getElementById("toggle-colour-palette");
+let currentPaletteIndex = 1;
+
+colourPaletteToggle.onclick = function () {
+    const root = document.querySelector(":root");
+    cyclePalette();
+
+    root.style.setProperty("--colour-one", colourPalettesDictionary[currentPaletteIndex][0]);
+    root.style.setProperty("--colour-two", colourPalettesDictionary[currentPaletteIndex][1]);
+    root.style.setProperty("--colour-three", colourPalettesDictionary[currentPaletteIndex][2]);
+    root.style.setProperty("--colour-four", colourPalettesDictionary[currentPaletteIndex][3]);
+    root.style.setProperty("--colour-five", colourPalettesDictionary[currentPaletteIndex][4]);
+};
+
+const colourPalettesDictionary = {
+    1: ["blue", "fuchsia", "yellow", "aqua", "red"],
+    3: ["red", "aqua", "yellow", "fuchsia", "red"],
+    2: ["#648FFF", "#DC267F", "#785EF0", "#FE6100", "#FFB000"],
+};
+
+function cyclePalette() {
+    if (currentPaletteIndex === 3) {
+        currentPaletteIndex = 1;
+    } else {
+        currentPaletteIndex += 1;
+    }
+}
